f0adefd8bccf7ac8389f12bd4dc89fec02e369d2
oniag <niag.souza1993@hotmail.com>
2020-08-26 11:51:55 -0300

    - Created structure.

---
+++ src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => (
+  <>
+    home
+  </>
+)
+
+export default Home
---
+++ src/routes/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Switch, Route, HashRouter } from 'react-router-dom'
+
+import Header from '../components/footer/Footer'
+import Footer from '../components/footer/Footer'
+
+import Home from '../pages/home/Home'
+
+const Routes = () => (
+  <>
+    <Header />
+    <main>
+      <HashRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </HashRouter>
+    </main>
+    <Footer />
+  </>
+)
+
+export default Routes
