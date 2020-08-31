da88adfee952365f694ac7b478be1e1bdf1d0519
Ivan Cunha <ivan.cunha@safeweb.com.br>
2020-08-30 21:38:57 -0300

    Adicionado um context para controle dos filmes

---
+++ src/contexts/movie.js
@@ -0,0 +1,33 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+} from 'react';
+
+import { API } from '../api/api'
+
+const MovieContext = createContext({ movies: [] });
+
+export const MovieProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+
+  const searchAPI = async (nameMovie) => {
+    const url = API(`${nameMovie}`)
+    const response = await fetch(url)
+    const data = await response.json()
+    setMovies(data.Search)
+  }
+
+  return (
+    <MovieContext.Provider value={{ movies, searchAPI }}>
+      {children}
+    </MovieContext.Provider>
+  );
+};
+
+export function useMovie() {
+  const context = useContext(MovieContext);
+
+  return context;
+}
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import GlobalStyle from './styles'
+import { MovieProvider }
+  from './contexts/movie'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MovieProvider>
+      <App />
+    </MovieProvider>
     <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
